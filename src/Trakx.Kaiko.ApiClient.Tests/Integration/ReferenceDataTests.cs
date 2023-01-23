75410da905b73a8a33aaf4018901a25e73cb4b96
Emanuel Gaspar <emanuel@cognip.com>
2023-01-23 19:56:44 +0000

    feat: add a test to download all instruments from Reference Data api and save legacy symbols in a CSV.

--- src/Trakx.Kaiko.ApiClient.Tests/Integration/ReferenceDataTests.cs
+++ src/Trakx.Kaiko.ApiClient.Tests/Integration/ReferenceDataTests.cs
@@ -51,4 +51,50 @@ public class ReferenceDataTests : IntegrationTestsBase
         response.Result.Data.Should().NotBeNull();
         response.Result.Data.Should().HaveCountGreaterThan(0);
     }
+
+    [InlineData(false)]
+    [Theory]
+    public async Task Download_legacy_symbols(bool download)
+    {
+        if (!download)
+        {
+            download.Should().BeFalse();
+            return;
+        }
+
+        var client = ServiceProvider.GetRequiredService<IInstrumentsClient>();
+        var response = await client.GetAllInstrumentsAsync();
+
+        using var writer = new StreamWriter("kaiko-legacy-symbols.csv");
+
+        void Write(params string[] items)
+        {
+            var line = string.Join(',', items); // .Select(p => $"'{p}")
+            writer.WriteLine(line);
+        }
+
+        Write("kaiko_legacy_symbol", "base_asset", "quote_asset");
+
+        var seen = new HashSet<string>();
+
+        var currencies = new[] { "usd", "usdc", "eur" };
+
+        var data = response.Result.Data
+            .Where(p => p.Class == "spot")
+            .Where(p => !string.IsNullOrWhiteSpace(p.Kaiko_legacy_symbol))
+            .Where(p => !p.Kaiko_legacy_symbol.StartsWith("0x"))
+            .Where(p => currencies.Contains(p.Quote_asset))
+            .DistinctBy(p => p.Kaiko_legacy_symbol)
+            .OrderBy(p => p.Kaiko_legacy_symbol);
+
+        foreach (var d in data)
+        {
+            if (seen.Contains(d.Kaiko_legacy_symbol)) continue;
+            Write(d.Kaiko_legacy_symbol, d.Base_asset, d.Quote_asset);
+        }
+
+        writer.Close();
+
+        download.Should().BeTrue();
+    }
 }
