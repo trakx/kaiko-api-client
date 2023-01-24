bf2d3a14c5d009994692ce23183eb3d8813f8c45
Emanuel Gaspar <emanuel@cognip.com>
2023-01-24 10:24:26 +0000

    Download legacy symbols (#29)
    * feat: add a test to download all instruments from Reference Data api and save legacy symbols in a CSV.
    
    * fix: remove unused seen HashSet, no longer needed as we have a DistinctBy
    
    * refactor: move Write method to private static

--- src/Trakx.Kaiko.ApiClient.Tests/Integration/ReferenceDataTests.cs
+++ src/Trakx.Kaiko.ApiClient.Tests/Integration/ReferenceDataTests.cs
@@ -51,4 +51,47 @@ public class ReferenceDataTests : IntegrationTestsBase
         response.Result.Data.Should().NotBeNull();
         response.Result.Data.Should().HaveCountGreaterThan(0);
     }
+
+    [InlineData(false)]
+    [Theory]
+    public async Task Download_legacy_symbols(bool download)
+    {
+        if (!download)
+        {
+            download.Should().BeFalse();
+            return;
+        }
+
+        var client = ServiceProvider.GetRequiredService<IInstrumentsClient>();
+        var response = await client.GetAllInstrumentsAsync();
+
+        using var writer = new StreamWriter("kaiko-legacy-symbols.csv");
+
+        WriteLineItems(writer, "kaiko_legacy_symbol", "base_asset", "quote_asset");
+
+        var currencies = new[] { "usd", "usdc", "eur" };
+
+        var data = response.Result.Data
+            .Where(p => p.Class == "spot")
+            .Where(p => !string.IsNullOrWhiteSpace(p.Kaiko_legacy_symbol))
+            .Where(p => !p.Kaiko_legacy_symbol.StartsWith("0x"))
+            .Where(p => currencies.Contains(p.Quote_asset))
+            .DistinctBy(p => p.Kaiko_legacy_symbol)
+            .OrderBy(p => p.Kaiko_legacy_symbol);
+
+        foreach (var d in data)
+        {
+            WriteLineItems(writer, d.Kaiko_legacy_symbol, d.Base_asset, d.Quote_asset);
+        }
+
+        writer.Close();
+
+        download.Should().BeTrue();
+    }
+
+    private static void WriteLineItems(StreamWriter writer, params string[] items)
+    {
+        var line = string.Join(',', items);
+        writer.WriteLine(line);
+    }
 }
