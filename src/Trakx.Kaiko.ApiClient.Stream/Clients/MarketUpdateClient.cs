b62a99a111af913062e2410dde23e4044ef11c78
Emanuel Gaspar <emanuel@cognip.com>
2023-05-03 10:56:54 +0100

    revert: remove duplicate MarketUpdateClientExtensions

--- src/Trakx.Kaiko.ApiClient.Stream/Clients/MarketUpdateClient.cs
+++ src/Trakx.Kaiko.ApiClient.Stream/Clients/MarketUpdateClient.cs
@@ -203,11 +203,3 @@ internal static class MarketUpdateClientExtensions
         [SdkUpdateType.ForceSnapshot] = StreamMarketUpdateType.ForceSnapshot,
     };
 }
-
-internal static class MarketUpdateClientExtensions
-{
-    internal static bool IsNullOrEmpty<T>(this IEnumerable<T> items)
-    {
-        return items == null || !items.Any();
-    }
-}
